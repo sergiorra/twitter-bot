9ceb39b719fde92a22dc64f27c8ad8bc09cfb1aa
Sergi Orra <sergiorra17@gmail.com>
2020-05-11 09:43:24 +0200

    bot ready

---
+++ README.md
@@ -0,0 +1,32 @@
+<div align="center">
+  <h1>Twitter Bot</h1>
+  <blockquote>Twitter Bot that follows back all your followers and likes certain number of tweets that contain some given keywords</blockquote>
+</div>
+
+## 📦 External libraries installation
+
+```
+// with Python 3
+pip3 install tweepy
+```
+
+You also need to have an account as a developer to interact with the Twitter API using your credentials. Check it out [here](https://developer.twitter.com/).
+
+## ⚙️ Usage and example
+
+- First argument: Keywords to search tweets
+- Second argument: Number of tweets you want to like
+
+```
+python main.py [Keywords] [Number of tweets]
+```
+
+- Example:
+
+```
+python main.py Python 5
+```
+
+## 📜 Documentation
+
+Check out the [tweepy Documentation](https://www.tweepy.org/).
---
+++ main.py
@@ -0,0 +1,36 @@
+# You will need to PIP INSTALL tweepy for this to work and also create a twitter API. Run this on your own machine, not in this Repl.
+import tweepy
+import sys
+import time
+
+auth = tweepy.OAuthHandler('<consumer_key>', '<consumer_secret>')
+auth.set_access_token('<access_token>', '<access_token_secret>')
+
+api = tweepy.API(auth)
+user = api.me()
+
+
+def limit_handle(cursor):
+    # Function to handle the Twitter API rate limit
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            time.sleep(300)
+
+
+for follower in limit_handle(tweepy.Cursor(api.followers).items()):
+    follower.follow()
+    print(f'{follower.name} followed!')
+
+search = sys.argv[1]
+numberOfTweets = int(sys.argv[2])
+
+for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+    try:
+        tweet.favorite()
+        print('Liked the tweet!')
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
